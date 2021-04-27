2967b9e496910b00f59579f6fe8ac5dd99d7c0bb
zhiwen.zhang <zhiwen.zhang@travelzen.com>
2021-04-27 16:55:07 +0800

    elasticsearch

---
+++ springboot/src/main/java/com/zzw/springboot/sys/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.zzw.springboot.sys.controller;
+
+import com.zzw.springboot.sys.entity.Book;
+import com.zzw.springboot.sys.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private  BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/book")
+    public Map<String,String> addBook(@RequestBody Book book){
+        bookService.addBook(book);
+        Map<String,String> map = new HashMap<>();
+        map.put("msg","ok");
+        return map;
+    }
+
+    @GetMapping("/book/search")
+    public SearchHits<Book> search(String key){
+        return bookService.searchBook1(key);
+    }
+}
+
---
+++ springboot/src/main/java/com/zzw/springboot/sys/service/BookService.java
@@ -0,0 +1,53 @@
+package com.zzw.springboot.sys.service;
+
+import com.zzw.springboot.sys.entity.Book;
+import com.zzw.springboot.sys.mapper.ESBookRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class BookService {
+
+    @Autowired
+    private  ESBookRepository esBookRepository;
+    @Autowired
+    private  TransactionTemplate transactionTemplate;
+
+//    public BookService(BookRepository bookRepository,
+//                       ESBookRepository esBookRepository,
+//                       TransactionTemplate transactionTemplate) {
+//        this.bookRepository = bookRepository;
+//        this.esBookRepository = esBookRepository;
+//        this.transactionTemplate = transactionTemplate;
+//    }
+
+    public void addBook(Book book) {
+        final Book saveBook = transactionTemplate.execute((status) ->
+                esBookRepository.save(book)
+        );
+        final Book esBook = new Book();
+        assert saveBook != null;
+        BeanUtils.copyProperties(saveBook, esBook);
+        esBook.setId(saveBook.getId() + "");
+        try {
+            esBookRepository.save(esBook);
+        }catch (Exception e){
+            log.error(String.format("保存ES错误！%s", e.getMessage()));
+        }
+    }
+
+    public List<Book> searchBook(String keyword){
+        return esBookRepository.findByTitleOrAuthor(keyword, keyword);
+    }
+
+    public SearchHits<Book> searchBook1(String keyword){
+        return esBookRepository.find(keyword);
+    }
+}
